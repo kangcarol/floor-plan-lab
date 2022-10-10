8700c5e76a72d1e66160844098e044149bbf2347
Carol Kang <otherhats@gmail.com>
2022-10-10 17:35:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FloorPlan from './FloorPlan';
+
+function App() {
+  return (
+    <div>
+        <FloorPlan />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Kitchen.jsx
@@ -0,0 +1,14 @@
+import Oven from "./Oven"
+import Sink from "./Sink"
+
+const Kitchen = (props) => {
+  return (
+    <div>
+      <h1>Kitchen</h1>
+      <div>Oven</div>
+      <div>Sink</div>
+    </div>
+  )
+}
+
+export default Kitchen
